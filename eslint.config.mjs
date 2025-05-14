d753164b7f6d5d817fde533b814e94225727b1fb
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2025-05-14 16:49:48 +0700

    style: eslint config style

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,10 +5,17 @@ import tseslint from "typescript-eslint";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { 
-    files: ["**/*.{js,mjs,cjs,ts}"] 
+    files: [
+      "**/*.{js,mjs,cjs,ts}"
+    ] 
   },
   { 
-    ignores: ["node_modules/", "dist/" , "build/", "logs/"], 
+    ignores: [
+      "node_modules/", 
+      "dist/" , 
+      "build/", 
+      "logs/"
+    ], 
   },
   { 
     languageOptions: { 
